b2b19ec0da396b22671b718106275182712fa010
BlackOokami <aramarza@mail.ru>
2020-06-20 09:34:02 +0300

    2

--- Project/BackroundSymbols.swift
+++ Project/BackroundSymbols.swift
@@ -14,7 +14,7 @@ let background = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
     func randomBackgroundKanji() -> String{
 
         for _ in 1...20{
-            _ = background.randomElement()!
+            _ = background.randomElement()
      
     }
         return "1" // чет у меня не вышло сделать нормальное возвращение

dba8b24bb20cfb1c3cd722b59619a0e1a90afc91
BlackOokami <aramarza@mail.ru>
2020-06-20 15:40:14 +0300

    1

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
         
+        func label(){
+            
+            
+        let label = UILabel(frame: CGRect(x: 10,y: 10,width: 50,height: 50))
+        label.center = CGPoint(x: 150,y: 150)
+        label.textAlignment = .center
+        label.text = "1"
+        self.view.addSubview(label)
+        
+        }
+        
+        label()
         
     }
     

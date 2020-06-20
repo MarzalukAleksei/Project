57fd87741ca9e7d0ba601a686ea8372807e36042
BlackOokami <aramarza@mail.ru>
2020-06-20 12:54:11 +0300

    7

--- Project/BackroundSymbols.swift
+++ Project/BackroundSymbols.swift
@@ -13,9 +13,9 @@ let background = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
 
 func randomBackgroundKanji() -> String{
     
-       let array = background.randomElement()!
+       let changeToString = background.randomElement()!
     
-        return String(array)
+        return String(changeToString)
     }
 
-let array = randomBackgroundKanji()
+

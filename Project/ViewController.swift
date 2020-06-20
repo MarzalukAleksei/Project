af48e2d2bc768654329d9c043837f5fcec4163ac
BlackOokami <aramarza@mail.ru>
2020-06-20 15:50:27 +0300

    2

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -14,20 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        func label(labelText: String){
-            
-            
-        let label = UILabel(frame: CGRect(x: 10,y: 10,width: 50,height: 50))
-        label.center = CGPoint(x: 150,y: 150)
-        label.textAlignment = .center
-        label.text = labelText
-        label.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-        self.view.addSubview(label)
-        
-        }
+       
         
         label(labelText: "12")
         
+    }
+    
+    
+    func label(labelText: String){
+        
+    let label = UILabel(frame: CGRect(x: 10,y: 10,width: 50,height: 50))
+    label.center = CGPoint(x: 60,y: 50)
+    label.textAlignment = .center
+    label.text = labelText
+    label.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+    self.view.addSubview(label)
+    
     }
     
 

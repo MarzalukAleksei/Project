e63752086b6e17c2eac6cb3b8b22bd24f91a45a1
CyrilRomanovsky <romanovski@wimix.by>
2020-06-21 16:51:58 +0300

    Update ViewController.swift

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -10,49 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let heightOfLabel: CGFloat = 50
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let countOfLabels = getCountOfLabels() // теперь тут количество лейблов на экране
+        
+        let heightOfLabel: CGFloat = 50
+        
+        let countOfLabels = getCountOfLabels(heightOfLabel: heightOfLabel) // теперь тут количество лейблов на экране
         // необходимо сейчас поместить ихз всех на экарн, для этого в функцию label добавь ещё один параметр - Y, чтобы можно было регулировать координату начала
-        var a = 1
-        for _ in 1...3{
-            label(labelText: "11", labelWidth: 50, labelHeight: 50, yCordinate: getLabelsY(labelCount: a, heightLabel: 50))
-            a += a
-        }
-        
-        label(labelText: "12", labelWidth: 50, labelHeight: heightOfLabel, yCordinate: 80)
-        
-       
-        
-    }
 
-//    func backGround(yElement: Int) {
-//        var
-//        for yCord in 1...yElement{
-//            label(labelText: "12", labelWidth: 50, labelHeight: heightOfLabel, yCordinate: getLabelsY(labelCount: yElement, heightLabel: 50))
-//
-//        }
-//
-//    }
+        for value in 0...countOfLabels { 
+            let arrayItem = randomBackgroundKanji()
+            let y = heightOfLabel * CGFloat(value)
+            label(labelText: arrayItem, labelWidth: 50, labelHeight: heightOfLabel, yCordinate: y)
+        }
+    }
     
-    func getCountOfLabels() -> Int {
+    func getCountOfLabels(heightOfLabel: CGFloat) -> Int {
         //UIScreen.main.bounds.height - высота всей вьюхи
         let count = Int( UIScreen.main.bounds.height / heightOfLabel ) // высота общей делить на высоту элемента ты получишь количество элементов которые влазят в экран перевести в инт, тк нужно целое количество
-        
         return count
-    }
-    
-    func getLabelsY(labelCount: Int, heightLabel: Int) -> CGFloat{// labelCount - строка, heightLabel - размер лейба для отступа
-        var yCordinate = 50
-        if labelCount > 1{
-            for _ in 2...labelCount{
-                yCordinate += heightLabel
-            }}
-        return CGFloat(yCordinate)
     }
     
     func label(labelText: String, labelWidth: CGFloat,labelHeight: CGFloat,yCordinate: CGFloat) {

0db6ae8ef7dd48bba9961b49a888124897505a3d
BlackOokami <aramarza@mail.ru>
2020-06-21 16:33:12 +0300

    1

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -13,18 +13,32 @@ class ViewController: UIViewController {
     let heightOfLabel: CGFloat = 50
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let countOfLabels = getCountOfLabels() // теперь тут количество лейблов на экране
         // необходимо сейчас поместить ихз всех на экарн, для этого в функцию label добавь ещё один параметр - Y, чтобы можно было регулировать координату начала
+        var a = 1
+        for _ in 1...3{
+            label(labelText: "11", labelWidth: 50, labelHeight: 50, yCordinate: getLabelsY(labelCount: a, heightLabel: 50))
+            a += a
+        }
         
-        label(labelText: "12", labelWidth: 50, labelHeight: heightOfLabel)
+        label(labelText: "12", labelWidth: 50, labelHeight: heightOfLabel, yCordinate: 80)
         
-        let testPrint = getLabelsY(labelCount: 1, heightLabel: 50)
-        print (testPrint)
+       
         
     }
 
+//    func backGround(yElement: Int) {
+//        var
+//        for yCord in 1...yElement{
+//            label(labelText: "12", labelWidth: 50, labelHeight: heightOfLabel, yCordinate: getLabelsY(labelCount: yElement, heightLabel: 50))
+//
+//        }
+//
+//    }
+    
     func getCountOfLabels() -> Int {
         //UIScreen.main.bounds.height - высота всей вьюхи
         let count = Int( UIScreen.main.bounds.height / heightOfLabel ) // высота общей делить на высоту элемента ты получишь количество элементов которые влазят в экран перевести в инт, тк нужно целое количество
@@ -33,7 +47,7 @@ class ViewController: UIViewController {
     }
     
     func getLabelsY(labelCount: Int, heightLabel: Int) -> CGFloat{// labelCount - строка, heightLabel - размер лейба для отступа
-        var yCordinate = 10
+        var yCordinate = 50
         if labelCount > 1{
             for _ in 2...labelCount{
                 yCordinate += heightLabel
@@ -41,8 +55,8 @@ class ViewController: UIViewController {
         return CGFloat(yCordinate)
     }
     
-    func label(labelText: String, labelWidth: CGFloat,labelHeight: CGFloat) {
-        let label = UILabel(frame: CGRect(x: 10,y: 10, width: labelWidth, height: labelHeight))
+    func label(labelText: String, labelWidth: CGFloat,labelHeight: CGFloat,yCordinate: CGFloat) {
+        let label = UILabel(frame: CGRect(x: 10,y: yCordinate, width: labelWidth, height: labelHeight))
         label.textAlignment = .center
         label.text = labelText
         label.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)

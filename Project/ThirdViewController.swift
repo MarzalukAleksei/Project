340adcdd939cc0585346dcedf63e14e1159b5ee6
BlackOokami <aramarza@mail.ru>
2020-06-23 22:06:39 +0300

    1

--- Project/ThirdViewController.swift
+++ Project/ThirdViewController.swift
@@ -8,23 +8,41 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        let heightOfLabel: CGFloat = 50
+            let widthOfLabel: CGFloat = 50
+            let xCordinate: CGFloat = 10
+            
+            let countOfLabels = getCountOfLabels(heightOfLabel: heightOfLabel) // теперь тут количество лейблов на экране
+            // необходимо сейчас поместить ихз всех на экарн, для этого в функцию label добавь ещё один параметр - Y, чтобы можно было регулировать координату начала
+            
+            
+            
+            for value in 0...countOfLabels {
+                let arrayItem = randomBackgroundKanji()
+                let y = heightOfLabel * CGFloat(value)
+                let newLabel = label(labelText: arrayItem, labelWidth: widthOfLabel, labelHeight: heightOfLabel, yCordinate: y, xCordinate: xCordinate)
+                view.addSubview(newLabel)
+                
+            }
+        }
+        
+        func getCountOfLabels(heightOfLabel: CGFloat) -> Int {
+            //UIScreen.main.bounds.height - высота всей вьюхи
+            let count = Int( UIScreen.main.bounds.height / heightOfLabel ) // высота общей делить на высоту элемента ты получишь количество элементов которые влазят в экран перевести в инт, тк нужно целое количество
+            return count
+        }
+        
+        func label(labelText: String, labelWidth: CGFloat,labelHeight: CGFloat,yCordinate: CGFloat, xCordinate: CGFloat) -> UILabel{
+            let label = UILabel(frame: CGRect(x: xCordinate,y: yCordinate, width: labelWidth, height: labelHeight))
+            label.textAlignment = .center
+            label.text = labelText
+            label.alpha = randomOne()
+            label.font.withSize(randomTwo())
+            return label
+        
+}
 }

2af68259547cd4b4545888c12362ab8274955078
BlackOokami <aramarza@mail.ru>
2020-06-24 14:10:37 +0300

    1

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class ViewController: SecondViewController {
-
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+        let heightLabel: CGFloat = 50
+        let widthLabel: CGFloat = 50
+        let xCordinate = UIScreen.main.bounds.width - widthLabel
+        let lCount = labelCount(heighLabel: heightLabel)
+        
+        
+        for item in 0...lCount{
+            let arrayItem = randomBackgroundKanji()
+            let yCordinate = heightLabel * CGFloat(item)
+            let rightLabel = rightSideLabel(labelText: arrayItem, xCor: xCordinate, yCor: yCordinate, heighLabel: heightLabel, widthLabel: widthLabel)
+            view.addSubview(rightLabel)
+        }
+       
+    }
+    
+    func labelCount (heighLabel: CGFloat) -> Int{
+        let count = UIScreen.main.bounds.height / heighLabel
+        return Int(count)
+    }
+    
+    func rightSideLabel(labelText: String,xCor: CGFloat,yCor: CGFloat,heighLabel: CGFloat, widthLabel: CGFloat) -> UILabel{ // проверить возвращение и без
+        let label = UILabel(frame: CGRect(x: xCor, y: yCor, width: widthLabel, height: heighLabel))
+        label.textAlignment = .center
+        label.text = labelText
+        label.alpha = 1
+        
+        return label
+    }
     
 }

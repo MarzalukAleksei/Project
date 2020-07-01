bc5f7d6a3a870bd7c8319f0e67497b3d9a058868
BlackOokami <aramarza@mail.ru>
2020-07-01 12:51:45 +0300

    1

---
+++ Project/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Project
+//
+//  Created by ブラック狼 on 01.07.2020.
+//  Copyright © 2020 ブラック狼. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+        
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+}
--- Project/CollectionViewController.swift
+++ Project/CollectionViewController.swift
@@ -15,6 +15,7 @@ class CollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -38,23 +39,26 @@ class CollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return kanaCount()
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+
+        cell.label.text = String(indexPath.row)
+        cell.backgroundColor = .blue
     
         return cell
     }
-
+    
+    
     // MARK: UICollectionViewDelegate
 
     /*

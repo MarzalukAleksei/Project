fc698c372e9942188af784e0e00716c4637f57b9
BlackOokami <aramarza@mail.ru>
2020-09-23 21:39:53 +0300

    up

---
+++ Project/Another/TestFile.swift
@@ -0,0 +1,14 @@
+//
+//  TestFile.swift
+//  Project
+//
+//  Created by ブラック狼 on 23.09.2020.
+//  Copyright © 2020 ブラック狼. All rights reserved.
+//
+
+import Foundation
+
+let testArray = getArrayFromCSV(fileName: "AllKanjiList", fileType: "csv")
+
+
+

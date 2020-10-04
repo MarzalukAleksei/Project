3302807a247dec801ef43056be6a473423879d0b
BlackOokami <aramarza@mail.ru>
2020-10-04 22:11:56 +0300

    -- upd test class

---
+++ Project/Source/Presentation/MainChosen/TestUIView.swift
@@ -0,0 +1,18 @@
+//
+//  TestUIControl.swift
+//  Project
+//
+//  Created by ブラック狼 on 04.10.2020.
+//  Copyright © 2020 ブラック狼. All rights reserved.
+//
+
+import UIKit
+
+class TestUIView: UIView {
+
+
+}
+
+class TestControl: UIControl {
+    
+}

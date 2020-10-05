ea285eadaeaf18a9c823e3aa604d82b141d63efa
BlackOokami <aramarza@mail.ru>
2020-10-05 16:31:00 +0300

    -- add xib, removeUIViewController

--- Project/Source/Presentation/MainChosen/TestUIView.swift
+++ Project/Source/Presentation/MainChosen/TestUIView.swift
@@ -1,18 +1,44 @@
 //
-//  TestUIControl.swift
+//  TestUIView.swift
 //  Project
 //
-//  Created by ブラック狼 on 04.10.2020.
+//  Created by ブラック狼 on 05.10.2020.
 //  Copyright © 2020 ブラック狼. All rights reserved.
 //
 
 import UIKit
 
 class TestUIView: UIView {
-
-
-}
-
-class TestControl: UIControl {
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
+    @IBOutlet weak var labelThree: UILabel!
+    
+    var view: UIView!
+    var nibName = "TestUIView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func loadFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func setup() {
+        view = loadFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+    }
     
 }

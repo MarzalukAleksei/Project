9756ed7db843e89d840eb103e685369236034b1b
BlackOokami <aramarza@mail.ru>
2020-10-07 22:23:52 +0300

    --upd What next?

--- Project/Source/Presentation/MainChosen/MainChosenViewController.swift
+++ Project/Source/Presentation/MainChosen/MainChosenViewController.swift
@@ -12,16 +12,24 @@ import UIKit
 class MainChosenViewController: BackgroundAnimatedViewController {
     
     @IBOutlet weak var customButtom: PushElement!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    
     
     let recogniser = UITapGestureRecognizer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         recogniser.addTarget(self, action: #selector(test))
         
+        
         customButtom.firstLabel.text = "label1"
         customButtom.secondLabel.text = "label1"
-        customButtom.addGestureRecognizer(recogniser)
+        image.layer.cornerRadius = image.bounds.width * 0.2
+        
+        
     }
     
     @objc func test() {

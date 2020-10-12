6deec68bb50b84df871d26dc90dc7de6c6d123f5
BlackOokami <aramarza@mail.ru>
2020-10-12 18:08:35 +0300

    -- upd: add DetailSearch Folder

---
+++ Project/Source/Presentation/Detail/SearchDetail/SearchDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchDetailViewController.swift
+//  Project
+//
+//  Created by ブラック狼 on 12.10.2020.
+//  Copyright © 2020 ブラック狼. All rights reserved.
+//
+
+import UIKit
+
+class SearchDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}

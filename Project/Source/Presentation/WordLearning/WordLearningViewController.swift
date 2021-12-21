2008619cb376e02f15451c8535e442f1bdcf46f3
BlackOokami <aramarza@mail.ru>
2021-12-21 16:18:03 +0300

    Add new ViewController - WordLearning

---
+++ Project/Source/Presentation/WordLearning/WordLearningViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WordLearningViewController.swift
+//  Project
+//
+//  Created by ブラック狼 on 21.12.2021.
+//  Copyright © 2021 ブラック狼. All rights reserved.
+//
+
+import UIKit
+
+class WordLearningViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
